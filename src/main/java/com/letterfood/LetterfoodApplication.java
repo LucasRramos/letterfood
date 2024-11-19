2ee33819de262550105b4567480a6d67528c6520
byasun <annaclarag6@gmail.com>
2024-11-19 20:57:56 -0300

    criação do seridor com o SparkJava

--- src/main/java/com/letterfood/LetterfoodApplication.java
+++ src/main/java/com/letterfood/LetterfoodApplication.java
@@ -8,29 +8,37 @@ import com.letterfood.repository.UsuarioRepository;
 import com.letterfood.service.RestauranteService;
 import com.letterfood.service.UsuarioService;
 
+import static spark.Spark.*;
+
 public class LetterfoodApplication {
 
     public static void main(String[] args) {
         try {
             // Configuração do MongoDB
             MongoConfig mongoConfig = new MongoConfig();
-
+    
             // Inicialização dos repositórios
             RestauranteRepository restauranteRepository = new RestauranteRepository();
             UsuarioRepository usuarioRepository = new UsuarioRepository();
-
+    
             // Inicialização dos serviços com os repositórios
             RestauranteService restauranteService = new RestauranteService(restauranteRepository);
             UsuarioService usuarioService = new UsuarioService(usuarioRepository);
-
+    
             // Inicialização dos controladores com os serviços
             UsuarioController usuarioController = new UsuarioController(usuarioService);
             RestauranteController restauranteController = new RestauranteController(restauranteService);
-
+    
             System.out.println("Aplicação Letterfood inicializada.");
-
+    
+            port(8080); // Define porta
+            get("/", (req, res) -> "Letterfood API Rodando");
+    
             // Fechar conexão ao encerrar a aplicação
-            Runtime.getRuntime().addShutdownHook(new Thread(mongoConfig::disconnect));
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                stop();
+                mongoConfig.disconnect();
+            }));
         } catch (Exception e) {
             System.err.println("Erro ao inicializar a aplicação: " + e.getMessage());
             e.printStackTrace();
